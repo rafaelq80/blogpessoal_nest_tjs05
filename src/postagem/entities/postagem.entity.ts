00847a6627eb8893238d41c2747151183f1d1ced
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-07-22 11:47:51 -0300

    CRUD Postagem - Parte 01

---
+++ src/postagem/entities/postagem.entity.ts
@@ -0,0 +1,22 @@
+﻿
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({name: "tb_postagens"}) // Criando a Tabela
+export class Postagem{
+
+    @PrimaryGeneratedColumn() // Chave Primária Autoincremental
+    id: number;
+
+    @IsNotEmpty() // Não aceitar titulo vazio
+    @Column({length: 100, nullable: false}) // Definir o tamanho e não aceitar valor nulo
+    titulo: string;
+
+    @IsNotEmpty()
+    @Column({length: 1000, nullable: false})
+    texto: string;
+
+    @UpdateDateColumn() // A data e a hora serão preenchidas automaticamente
+    data: Date;
+
+}
---
+++ src/postagem/postagem.module.ts
@@ -0,0 +1,14 @@
+﻿import { Module } from "@nestjs/common";
+import { Postagem } from "./entities/postagem.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { PostagemService } from "./services/postagem.service";
+import { PostagemController } from "./controllers/postagem.controller";
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Postagem])],
+    providers: [PostagemService],
+    controllers: [PostagemController],
+    exports: [TypeOrmModule]
+})
+
+export class PostagemModule { }
